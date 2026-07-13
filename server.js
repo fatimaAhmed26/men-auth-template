52a2c8f21201ae520546140e56a607a4956016c0
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-07-13 10:55:10 +0300

    adding the files and the db connction

---
+++ server.js
@@ -0,0 +1,33 @@
+const dns = require("node:dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"])
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+const path= require('path')
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+
+// Set the port from environment variable or default to 3000
+const port = process.env.PORT ? process.env.PORT : "3000";
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+// Middleware to parse URL-encoded data from forms
+app.use(express.urlencoded({ extended: false }));
+// Middleware for using HTTP verbs such as PUT or DELETE
+app.use(methodOverride("_method"));
+
+ app.use(express.static(path.join(__dirname, "public")));
+// Morgan for logging HTTP requests
+app.use(morgan('dev'));
+
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+});
