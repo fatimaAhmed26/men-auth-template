54b9f134c4d2fbdca8773c3784c727e4545683eb
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-07-13 18:00:53 +0300

    sign in form

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User =require('../models/user')
+const bcrypt = require('bcrypt')
 const home = (req,res)=>{
     res.send('welcome')
 }
@@ -6,16 +7,47 @@ const showSignUpForm = (req,res)=>{
 res.render('auth/sign-up.ejs')
 }
 const signUp =async (req,res)=>{
+    const userInTheDatabase = await User.findOne({
+        username: req.body.username
+    })
+    if(userInTheDatabase){
+        return res.send('User already taken.')
+    }
   let userDate={}
   userDate.username = req.body.username
   userDate.password = req.body.password
+  const hasHadPassword = bcrypt.hashSync(req.body.password, 10)
+  userDate.password = userDate.username
   const user =await User.create(userDate)
     res.send(user)
+}
+const showSignInForm = (req , res) =>{
+    res.render('auth/sign-in.ejs')
+}
+const singIn = async (req , res) => {
+    const userInDatabase = await User.findOne({
+        username: req.body.username
+    })
+
+    if (!userInDatabase) {
+        return res.send('User does not exist.')
+    }
+
+    const validPassword = bcrypt.compareSync(req.body.password, userInDatabase.password)
+
+    if(!validPassword) {
+        return res.send('Login failed. Please try again.')
+    }
+
+    res.send('sing in rout')
 }
 
 module.exports={
     home,
     showSignUpForm,
-    signUp
+    signUp ,
+    showSignInForm,
+    singIn,
+
 
 }
--- server.js
+++ server.js
@@ -34,6 +34,8 @@ app.get('/',(req, res)=>{
 app.get('/auth/home',authCtrl.home )
 app.get('/auth/sign-up' , authCtrl.showSignUpForm)
 app.post('/auth/sign-up' ,authCtrl.signUp)
+app.get('/auth/sign-in',authCtrl.showSignInForm)
+app.post('/auth/sign-in', authCtrl.singIn)
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
