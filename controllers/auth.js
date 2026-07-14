9e6479a92853f6d5fb5c5ed4ff6910f54c3a851e
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-07-14 13:02:24 +0300

    addong the fruits app

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,47 @@
-const User =require('../models/user')
+const User = require('../models/user')
 const bcrypt = require('bcrypt')
-const home = (req,res)=>{
+
+const home = (req, res) => {
     res.send('welcome')
 }
-const showSignUpForm = (req,res)=>{
-res.render('auth/sign-up.ejs')
+
+const showSignUpForm = (req, res) => {
+    res.render('auth/sign-up.ejs')
 }
-const signUp =async (req,res)=>{
-    const userInTheDatabase = await User.findOne({
+
+const signUp = async (req, res) => {
+    const userInDatabase = await User.findOne({
         username: req.body.username
     })
-    if(userInTheDatabase){
-        return res.send('User already taken.')
+
+    if (userInDatabase) {
+        return res.send('Username already taken.')
     }
-  let userDate={}
-  userDate.username = req.body.username
-  userDate.password = req.body.password
-  const hasHadPassword = bcrypt.hashSync(req.body.password, 10)
-  userDate.password = userDate.username
-  const user =await User.create(userDate)
+
+    let userData = {}
+    userData.username = req.body.username
+
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+    userData.password = hashedPassword
+
+    const user = await User.create(userData)
+    
+        req.session.user = {
+        username: userInDatabase.username,
+        _id: userInDatabase._id
+    }
+    req.session.save(()=>{
+      res.redirect('/')  
+    })
+    
     res.send(user)
 }
-const showSignInForm = (req , res) =>{
+
+const showSignInForm = (req, res) => {
     res.render('auth/sign-in.ejs')
 }
-const singIn = async (req , res) => {
+
+const signIn = async (req, res) => {
     const userInDatabase = await User.findOne({
         username: req.body.username
     })
@@ -39,15 +56,30 @@ const singIn = async (req , res) => {
         return res.send('Login failed. Please try again.')
     }
 
-    res.send('sing in rout')
+    req.session.user = {
+        username: userInDatabase.username,
+        _id: userInDatabase._id
+    }
+    req.session.save(()=>{
+      res.redirect('/')  
+    })
+    
+}
+const signOut = async (req, res) => {
+    req.session.destroy(()=>{
+         res.redirect('/')
+    }
+
+    )
+   
 }
 
-module.exports={
+
+module.exports = {
     home,
     showSignUpForm,
-    signUp ,
+    signUp,
     showSignInForm,
-    singIn,
-
-
+    signIn,
+    signOut,
 }
--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"])
-
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const app = express();
-const path= require('path')
+const path = require('path')
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
+const session = require('express-session')
+const{MongoStore} = require('connect-mongo')
+const fruitsCtrl =require('./controllers/fruits.controllers.js')
 const authCtrl = require('./controllers/auth')
+const Fruit= require('./models/fruits.js');
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
 
+app.use(express.static(path.join(__dirname, 'public')))
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on("connected", () => {
@@ -23,19 +27,55 @@ mongoose.connection.on("connected", () => {
 app.use(express.urlencoded({ extended: false }));
 // Middleware for using HTTP verbs such as PUT or DELETE
 app.use(methodOverride("_method"));
-
- app.use(express.static(path.join(__dirname, "public")));
 // Morgan for logging HTTP requests
 app.use(morgan('dev'));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store:MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
 
-app.get('/',(req, res)=>{
-    res.render('home.ejs')
+}))
+
+app.get('/', (req, res) => {
+    res.render('home.ejs', {
+        user: req.session.user,
+    })
 })
-app.get('/auth/home',authCtrl.home )
-app.get('/auth/sign-up' , authCtrl.showSignUpForm)
-app.post('/auth/sign-up' ,authCtrl.signUp)
-app.get('/auth/sign-in',authCtrl.showSignInForm)
-app.post('/auth/sign-in', authCtrl.singIn)
+
+app.get('/auth/home', authCtrl.home)
+app.get('/auth/sign-up', authCtrl.showSignUpForm )
+app.post('/auth/sign-up', authCtrl.signUp)
+app.get('/auth/sign-in', authCtrl.showSignInForm)
+app.post('/auth/sign-in', authCtrl.signIn)
+app.delete('/auth/sign-out', authCtrl.signOut)
+
+app.get('/dashboard', async (req, res) => {
+    if (!req.session.user){
+        return res.redirect('/auth/sign-in')
+    }
+    res.render('dashboard.ejs',{
+        user: req.session.user
+    })
+})
+
+//form for creating a new fruit 
+app.get('/fruits/new', async (req,res)=>{
+res.render('new.ejs')
+})
+//post /fruits 
+app.post('/fruits',fruitsCtrl.newFruit)
+    //get all fruits /fruits 
+    app.get('/fruits',fruitsCtrl.index)
+    //get show route 
+    app.get('/fruits/:fruitId' ,fruitsCtrl.show)
+
+    app.delete('/fruits/:fruitId', fruitsCtrl.deleteFruit)
+    // edit (put)
+    app.get('/fruits/:fruitId/edit',fruitsCtrl.edit)
+    app.put('/fruits/:fruitId',fruitsCtrl.update)
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
